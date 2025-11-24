bb27415f7b5b2121a80a4e75e9933990e638a42e
Sergey Bondar <abobopw@gmail.com>
2025-11-24 19:22:02 +0100

    fix: 123

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek'
-const string = 'фффdaвфыfsdfsdвsdaф';
+const string = 'фффdaвфыfsdfsdвsdasdadaф';
 
 console.log('!I am an npm package', lol, string)
 
