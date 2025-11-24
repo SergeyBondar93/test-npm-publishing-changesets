7d53952278963eb68dcc1dd76b1143d95ff7bcef
Sergey Bondar <abobopw@gmail.com>
2025-11-24 19:33:13 +0100

    fix: fix bug 2

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek'
-const string = 'loler update 1';
+const string = 'loler update 1. fix bug 2';
 
 console.log('!I am an npm package', lol, string)
 
