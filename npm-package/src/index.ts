52e13ae571b9c11af378dc5269f4bc088f2ad977
SergeyBondar93 <42748618+SergeyBondar93@users.noreply.github.com>
2025-11-25 13:53:02 +0100

    Ra/fasd (#20)
    * fix: 123
    
    * fix: fdshkj

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek'
-const string = 'i am a new1!da!';
+const string = 'i am a new1!dadas!';
 
 console.log('!I am an npm packacbvbvcge', lol, string)
 
