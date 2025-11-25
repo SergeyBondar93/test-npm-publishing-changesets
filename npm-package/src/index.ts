b562720b3d21cf786a310cd5b4c144745806b57a
Sergey Bondar <abobopw@gmail.com>
2025-11-25 12:01:14 +0100

    feat: 12344

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek'
-const string = 'loler updatfsdfsedasdas 1. fix bug dsadadasdasdаыва11s2';
+const string = 'loler updatfsdfsdasdass2121221edasdas 1. fix bug dsadadasdasdаыва11s2';
 
 console.log('!I am an npm packacbvbvcge', lol, string)
 
