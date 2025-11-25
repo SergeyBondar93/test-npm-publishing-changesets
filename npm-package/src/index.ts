c8a8c7b7509f9962f12c85ed062fa8259c126974
SergeyBondar93 <42748618+SergeyBondar93@users.noreply.github.com>
2025-11-25 12:40:44 +0100

    fix: fix release (#9)

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek'
-const string = 'loler updatfsdfsdasdass2121221edasdas 1. fix bug dsadadasdasdаыва11s2';
+const string = 'loler updatfsdfsdasdass2121221edasdas 1. fix bug dsadadasdasdаыва11s2dasdas212121';
 
 console.log('!I am an npm packacbvbvcge', lol, string)
 
