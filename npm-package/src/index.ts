2e9975c4a52222b4af8ac6970a43e173144fdf6b
Sergey Bondar <abobopw@gmail.com>
2025-11-25 11:58:02 +0100

    fix: 121231

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek'
-const string = 'loler update 1. fix bug dsada11s2';
+const string = 'loler update 1. fix bug dsadadasdasdаыва11s2';
 
 console.log('!I am an npm packacbvbvcge', lol, string)
 
