412b86977433dc8a1298669a0dbdc3820886a6ea
Sergey Bondar <abobopw@gmail.com>
2025-11-25 13:11:45 +0100

    fix: clean u

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek'
-const string = 'i am a new 1.3 hopefully. idk. lol. kek. aa: 123';
+const string = 'i am a new 1.3 hopefully.';
 
 console.log('!I am an npm packacbvbvcge', lol, string)
 
