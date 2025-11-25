55b3c9996d43ee9947bc4368992bb1003b273c2e
Sergey Bondar <abobopw@gmail.com>
2025-11-25 12:52:24 +0100

    fix: remove extra line

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek'
-const string = 'ddd1';
+const string = 'i am a new 1.3 hopefully';
 
 console.log('!I am an npm packacbvbvcge', lol, string)
 
