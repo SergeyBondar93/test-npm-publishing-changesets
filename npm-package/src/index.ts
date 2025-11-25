46bb80dd8e22a9faf5b377ef88e947da62a91fc3
Sergey Bondar <abobopw@gmail.com>
2025-11-25 13:12:04 +0100

    fix: kek

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek'
-const string = 'i am a new 1.3 hopefully. lol';
+const string = 'i am a new 1.3 hopefully. lol. kek.';
 
 console.log('!I am an npm packacbvbvcge', lol, string)
 
