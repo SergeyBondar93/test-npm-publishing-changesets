c0615c704681c6c86493ec84ec4d134a296e9416
Sergey Bondar <abobopw@gmail.com>
2025-11-25 16:34:56 +0100

    dsa

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek1'
-const string = 'i am a neвфыw';
+const string = 'i am a neвdasdфыw';
 
 console.log('!I am an npm packacbvbvcge', lol, string)
 
