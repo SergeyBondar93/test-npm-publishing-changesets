4b6538a63a55feef1d5e6d5f2834053ac85f5afa
Sergey Bondar <abobopw@gmail.com>
2025-11-25 12:52:00 +0100

    chore: lol

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek'
-const string = 'loler udaspdatfsdfsdasdass2121221eаывdasdas 1. fix bug dsadadasdasdаыва11s2dasdas2121211111';
+const string = 'ddd1';
 
 console.log('!I am an npm packacbvbvcge', lol, string)
 
