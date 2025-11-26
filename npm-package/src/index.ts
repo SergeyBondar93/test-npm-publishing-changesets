c2eee866cd4c8805e49145ac0c2f7980f92efb98
Sergey Bondar <abobopw@gmail.com>
2025-11-26 20:27:07 +0100

    feat: add feature

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek1'
-const string = 'i am a new. new feat.12.12.456';
+const string = 'i am a new. new feat.12.12.456. feature branch';
 
 console.log('!I am an npm packacbvbvcge', lol, string)
 
