06341052a88a2a79277fe7d900bafdf4915d09b1
SergeyBondar93 <42748618+SergeyBondar93@users.noreply.github.com>
2025-11-26 11:07:56 +0100

    Tester/test refresh (#34)
    * dsad
    
    * feat: new lol

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek1'
-const string = 'i am a new. new feat';
+const string = 'i am a new. new feat.12';
 
 console.log('!I am an npm packacbvbvcge', lol, string)
 
