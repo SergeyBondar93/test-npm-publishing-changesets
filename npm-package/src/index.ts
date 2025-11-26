9ab5b0f20ad09d71a8e589e1f1c3d78caf226881
SergeyBondar93 <42748618+SergeyBondar93@users.noreply.github.com>
2025-11-26 20:31:30 +0100

    Tester/test branch release (#35)
    * fix: 123
    
    * feat: lol feat
    
    * feat: add a new release posibility
    
    * feat: add feature
    
    * fix: final fix

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek1'
-const string = 'i am a new. new feat.12';
+const string = 'i am a new. new feat.12.12.456. feature branch. final update';
 
 console.log('!I am an npm packacbvbvcge', lol, string)
 
