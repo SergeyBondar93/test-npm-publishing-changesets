5c619cc983d575d47efc2f0450c17a63787c9374
Sergey Bondar <abobopw@gmail.com>
2025-11-26 09:55:16 +0100

    fix: fgdsj

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,7 +1,7 @@
 import { B } from '@repo/shared';
 
 const lol = 'kek1'
-const string = 'i am a new.12321';
+const string = 'i am a new.12321.dsa';
 
 console.log('!I am an npm packacbvbvcge', lol, string)
 
