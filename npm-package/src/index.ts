91786761ddf691e395b7a977ed4b4173155bcec3
SergeyBondar93 <42748618+SergeyBondar93@users.noreply.github.com>
2026-03-14 12:15:00 +0100

    Test/test (#44)
    * test: fix
    
    * fix: fix
    
    * fix: test
    
    * fix: test test
    
    * chore: test

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,6 +1,6 @@
 import { B } from '@repo/shared';
 
-const lol = 'kek1'
+const lol = 'removed semantic release!'
 const string = 'i am a new. new feat.12.12.456. feature branch. final update';
 
 console.log('!I am an npm packacbvbvcge', lol, string)
